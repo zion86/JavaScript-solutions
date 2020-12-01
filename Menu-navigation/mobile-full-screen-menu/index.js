747a049ea02f0dfbb23db831fa8b687eda6ab92d
Oleg V <oleg.vintonuk@gmail.com>
2020-12-01 23:33:29 +0200

    update css js

--- Menu-navigation/mobile-full-screen-menu/index.js
+++ Menu-navigation/mobile-full-screen-menu/index.js
@@ -3,9 +3,11 @@
 var icon = document.querySelector('.icon');
 var menu = document.querySelector('.menu');
 var header = document.querySelector('.header');
+var body = document.querySelector('body');
 icon.addEventListener('click', function () {
   menu.classList.toggle('menu--full');
   header.classList.toggle('header--full-height');
+  body.classList.toggle('block-scroll');
 });
 
 
@@ -14,6 +16,7 @@ var widthChange = function widthChange(mq) {
   if (mq && header.classList.contains('header--full-height')) {
     menu.classList.toggle('menu--full');
     header.classList.toggle('header--full-height');
+    body.classList.toggle('block-scroll');
   }
 };
 
